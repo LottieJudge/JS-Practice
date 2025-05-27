ac8586917a6c767583b6bc3da1fc3eb3cbcad1dc
lottiejudge <lottielucyjudge@gmail.com>
2025-05-27 16:21:57 +0100

    testing start

---
+++ manualTesting.js
@@ -0,0 +1,1 @@
+// Manual testing is done by humans interacting with the system. Purposely trying to break it but using it in unexpected ways, - hard for developers to do themselves cos they know how to use the systems 
