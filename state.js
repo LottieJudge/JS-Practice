d36f680e1fc6a8ff0243376978f1c65b266cce57
lottiejudge <lottielucyjudge@gmail.com>
2025-06-26 08:51:32 +0100

    react, hooks and state

--- state.js
+++ state.js
@@ -1,4 +1,20 @@
 // State with codecademy 
 
+import { useState } from "react"
+
 // Using hooks in react. React hooks are functions that let us manage the internal state of components and handle post rendering side effects. Using hooks we can determine what the user is shown based on the state. E.G are they logged in y/n? if y then they should have full access to their profile if no then the log in page and home page will do just fine! 
+
+// Built in Hooks 
+
+const hookUseState = "the useState hook allows us to track the state of an app in a function component.  "
+
+const hookUseEffect = ""
+
+const hookUseContext = ""
+
+const hookUseReducer = ""
+
+const hookUseRef = ""
+
+
 
