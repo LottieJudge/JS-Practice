008cc6070c2111fda10629443e2642fe4935d6f6
lottiejudge <lottielucyjudge@gmail.com>
2025-07-14 16:30:14 +0100

    counter function

--- state _react/vite-project/src/counter.jsx
+++ state _react/vite-project/src/counter.jsx
@@ -1,1 +1,15 @@
-imprta
+import React, { useState } from "react";
+
+export default function Counter(){
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(prevCount => prevCount +1 )
+  }
+
+  return (
+    <div>
+      <p>wow you've clicked the button: {count} times</p>
+      <button onClick={increment}></button>
+    </div>
+  )
+}
